15f73977fbb731a368b67a06bd0d4c9581fa90ce
DEMENEZ Hugo <hugo.demenez@mazars.fr>
2023-03-01 17:00:43 +0100

    boilerplate

--- components/timeline.js
+++ components/timeline.js
@@ -1,10 +1,10 @@
 const ctx = document.getElementById('timeline');
 
 const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: ['Value', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
     datasets: [{
     label: '',
-    data: [1, 19, 3, 5, 2, 3],
+    data: [[8,10], [8.2,15], [10,15], [10,15], [10,15], [10,15]],
     borderWidth: 1
     }]
 }
@@ -13,9 +13,29 @@ const timeline = new Chart(ctx, {
         type: 'bar',
         data: data,
         options: {
+            plugins:{
+                legend:{
+                    display:false,
+                },
+            },
             scales : {
+                y:{
+                    display:false,
+                },
                 x:{
                     position:"top",
+                    ticks:{
+                        borderDash :[1,8],
+                        // Include a dollar sign in the ticks
+                        callback: function(value, index, ticks) {
+                            if (value<10){
+                                return '0'+value+':00'
+                            }
+                            else{
+                                return value+':00'
+                            }
+                        }
+                    }
                 },
             },
             indexAxis: 'y',
