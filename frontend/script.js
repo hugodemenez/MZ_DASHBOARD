29ef74b548c1ed3e8ca59c2156f7d985a78c206b
DEMENEZ Hugo <hugo.demenez@mazars.fr>
2023-03-08 17:08:28 +0100

    User interface

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,13 @@
-document.getElementById("dialog-box")
+const dialog = document.getElementById("dialog-box")
+const zone1 = document.getElementById("zone1");
+
 
 function open_dialog_box(){
-    document.getElementById("dialog-box").showModal();
+    dialog.showModal();
 }
 
 function close_dialog_box(){
-    document.getElementById("dialog-box").close();
+    dialog.close();
     const content = {
         "username": document.getElementById("username").value,
         "password": document.getElementById("password").value
@@ -23,15 +25,9 @@ function close_dialog_box(){
     fetch(request).then(function(response) {
         return response.json();
     }).then(function(response) {
-        console.log(response);
-        const zone1 = document.getElementById("zone1");
         try{
-            const p = document.createElement("p");
             response.forEach(element => {
-                const card = document.createElement('div');
-                card.className = "card";
-                card.append(element.AFFAIRES,p);
-                zone1.prepend(card,p);
+                create_card(element);
             });
         }
         catch(error){
@@ -72,14 +68,10 @@ function fetch_database(){
         return response.json();
     }).then(function(response) {
         console.log(response)
-        const zone1 = document.getElementById("zone1");
         try{
-            const p = document.createElement("p");
+            
             response.forEach(element => {
-                const card = document.createElement('div');
-                card.className = "card";
-                card.append(element.AFFAIRES,p);
-                zone1.prepend(card,p);
+                create_card(element)
             });
         }
         catch(error){
@@ -88,87 +80,35 @@ function fetch_database(){
     });
 }
 
-const data = [
-    {
-        "AFFAIRES": "CSF220087 - OSCAR LAMBRET-31/12-CSF-AUDITLEGAL-2022",
-        "Total": 7
-    },
-    {
-        "AFFAIRES": "CSF230149 - MZ GENERIX-31/03-CSF-AUDITLEGAL-2023",
-        "Total": 3
-    },
-    {
-        "AFFAIRES": "E003ABS-NR - ABSENCES NON REMUNEREES",
-        "Total": 448
-    },
-    {
-        "AFFAIRES": "E003ABSEN - ABSENCES",
-        "Total": 7
-    },
-    {
-        "AFFAIRES": "E003CONGES - CONGES",
-        "Total": 35
-    },
-    {
-        "AFFAIRES": "E003DOC007 - E003DOC_Temps documentation",
-        "Total": 21
-    },
-    {
-        "AFFAIRES": "E003FOR002 - E003FOR_Particip form interne",
-        "Total": 30.5
-    },
-    {
-        "AFFAIRES": "E003SS-AFF - E003 SANS AFFECTATION",
-        "Total": 4
-    },
-    {
-        "AFFAIRES": "EBE220059 - MZ TICKET FOR THE MOON-31/12-EBE-AUDITLEGAL-2022",
-        "Total": 10
-    },
-    {
-        "AFFAIRES": "EBE220065 - RABOT DUTILLEUL CONSTRUCTION-31/12-EBE-AUDITLEGAL-2022",
-        "Total": 19
-    },
-    {
-        "AFFAIRES": "EBE220066 - IRCEM-31/12-EBE-AUDITLEGAL-2022",
-        "Total": 56.5
-    },
-    {
-        "AFFAIRES": "EBE220234 - MZ MOBIVIA-30/09-EBE-AUDITLEGAL-2022",
-        "Total": 139
-    },
-    {
-        "AFFAIRES": "EBE220235 - MZ MOBIVIA CONSO-30/09-EBE-AUDITLEGAL-2022",
-        "Total": 2
-    },
-    {
-        "AFFAIRES": "EBE220236 - MZ NORAUTO FRANCE-30/09-EBE-AUDITLEGAL-2022",
-        "Total": 7
-    },
-    {
-        "AFFAIRES": "EBE220275 - GETLINK-31/12-EBE-AUDITLEGAL-2022",
-        "Total": 17
-    },
-    {
-        "AFFAIRES": "JPV220004 - ADVENS-31/12-JPV-AUDITLEGAL-2022",
-        "Total": 1
-    },
-    {
-        "AFFAIRES": "JPV220826 - EDHEC B SCHOOL-31/08-JPV-AUDITLEGAL-2022",
-        "Total": 12
-    },
-    {
-        "AFFAIRES": "JPV221501 - SAS DECATHLON FRANCE-31/12-JPV-AUDITLEGAL-2022",
-        "Total": 77
-    },
-    {
-        "AFFAIRES": "Total Temps",
-        "Total": 896
-    }
-]
-
-
 fetch_database();
 
+dossiers = [
+    "DECATHLON",
+    "ADVENS",
+]
 
+function create_card(data){
+    dossiers.forEach(dossier => {
+        if (data.AFFAIRES.includes(dossier)){
+            const card = document.createElement('div');
+            card.className = "card";
+
+            // Add title name
+            const title = document.createElement("h1");
+            title.className = "card-title";
+            title.append(dossier)
+
+            // Add total hours
+            const status = document.createElement("p");
+            status.className = "card-status";
+            status.append(data.Total)
+
+
+            card.append(title);
+            card.append(status);
+            zone1.prepend(card);
+        }
+        }
+    );
+}
 
